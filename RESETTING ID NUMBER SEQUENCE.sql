17318d4b331cfaa77e8234520f1e64597d0dfdea
Ron Michael R. Umayan <umayan.ronmichael@gmail.com>
2017-06-01 12:20:19 +0800

    Added error logs on migrating forum reply

--- RESETTING ID NUMBER SEQUENCE.sql
+++ RESETTING ID NUMBER SEQUENCE.sql
@@ -4,11 +4,13 @@ l_val NUMBER;
 
 BEGIN
 
-    execute immediate 'select SEQ_DOWNLOADS_CONTENT_ID.nextval from dual' INTO l_val;
+    execute immediate 'select SEQ_NEWS_ID.nextval from dual' INTO l_val;
 
-    execute immediate  'alter sequence SEQ_DOWNLOADS_CONTENT_ID increment by -' || l_val || ' minvalue 0';
+    execute immediate  'alter sequence SEQ_NEWS_ID increment by -' || l_val || ' minvalue 0';
 
-    execute immediate 'select SEQ_DOWNLOADS_CONTENT_ID.nextval from dual' INTO l_val;
+    execute immediate 'select SEQ_NEWS_ID.nextval from dual' INTO l_val;
 
-    execute immediate 'alter sequence SEQ_DOWNLOADS_CONTENT_ID increment by 1 minvalue 0';
+    execute immediate 'alter sequence SEQ_NEWS_ID increment by 1 minvalue 0';
 END;
+
+
