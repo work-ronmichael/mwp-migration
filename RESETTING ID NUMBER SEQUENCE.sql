f1d84b7b36c3e7aefe88bc219ceeb68f5faff0d8
Ron-Michael R. Umayan <umayan.ronmichael@gmail.com>
2017-11-16 11:45:56 +0800

    Added pl sql for resetting all sequences

--- RESETTING ID NUMBER SEQUENCE.sql
+++ RESETTING ID NUMBER SEQUENCE.sql
@@ -14,3 +14,18 @@ BEGIN
 END;
 
 
+
+--ALL SEQUENCES
+DECLARE
+    l_val NUMBER;
+BEGIN
+    for x in (select sequence_name from USER_SEQUENCES)
+    loop
+        execute immediate 'select '|| x.sequence_name ||'.nextval from dual' INTO l_val;
+        execute immediate 'alter sequence '|| x.sequence_name ||' increment by -' || l_val || ' minvalue 0';
+        execute immediate 'select '|| x.sequence_name ||'.nextval from dual' INTO l_val;
+        execute immediate 'alter sequence  '|| x.sequence_name ||' increment by 1 minvalue 0';
+    end loop;
+END;
+
+
